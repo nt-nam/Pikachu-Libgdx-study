a0c091f92d03fd42f00b67b3fb6e9ca7bc526fd0
Nam <95182414+Thanhman5@users.noreply.github.com>
2025-02-15 08:46:39 +0700

    first commit

---
+++ core/src/com/mygdx/game/PikachuGame.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class PikachuGame extends Game {
+  Stage stage;
+
+  public PikachuGame() {
+
+    super();
+  }
+  @Override
+  public void create() {
+
+  }
+
+
+}
